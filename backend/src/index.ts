55e6d3d6231751c24723fe2e0be40d0a77a03bed
chandima2000 <hychandima2000@gmail.com>
2024-03-05 10:36:07 +0530

    did some changes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,13 @@
 import express, {Request, Response } from 'express';
 import cors from 'cors';
 import 'dotenv/config';
-import exp from 'constants';
+
 
 
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true })); 
 app.use(cors()); 
 
 app.get("/api/test", async (req:Request, res:Response) => {
